30de4da1c83a8ae3ec4f3c29223df5c0975bb670
Onome Oyibo <onome@dyn1205-135.wlan.ic.ac.uk>
2017-07-31 12:57:34 +0100

    lol

---
+++ createSingleComponentRegion.m
@@ -0,0 +1,6 @@
+function [region_with_functions] = createSingleComponentRegion(variables_string,varargin)
+    s = nargin - 1;
+    region_with_functions = cell(1,s);
+    for i = 1:s
+        region_with_functions{1,i} = createSymbolicFunction(variables_string,varargin{1,i});
+    end
--- untitled.m
+++ untitled.m
@@ -14,3 +14,13 @@ r3 = createRegion('x,y,z','2*(x+y+z)^2 +17');
 set_of_region_functions = insertRegionFunctionInfo(set_of_region_functions,r1,1);
 set_of_region_functions = insertRegionFunctionInfo(set_of_region_functions,r2,2);
 set_of_region_functions = insertRegionFunctionInfo(set_of_region_functions,r3,3);
+
+[hyperplanes,~,set_of_region_functions,region_info_cell] = createDomainWithRegions('x,y','x==0','x==5');
+r1 = createRegion('x,y','x^2-x+y^2-6*y+13');
+r2 = createRegion('x,y','x^2 - 4*x+13+y^2-6*y');
+r3 = createRegion('x,y','x+13+y^2-6*y');
+set_of_region_functions = insertRegionFunctionInfo(set_of_region_functions,r1,1);
+set_of_region_functions = insertRegionFunctionInfo(set_of_region_functions,r2,2);
+set_of_region_functions = insertRegionFunctionInfo(set_of_region_functions,r3,3);
+[xk,iteration]= optimiseSubgradientDescentMethod(set_of_region_functions,region_info_cell,hyperplanes,'x,y',[5;-5],0.01,0.1);
+
